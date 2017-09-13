41c979f1613be23400ed2b9f1e6cdecc997317c6
ldeininger <deininger.luca@gmail.com>
2017-09-13 13:54:45 +0200

    added PluginNetworkInterop

---
+++ PluginNetworkInterop/PluginNetworkInterop/Python/NetworkProcessing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BaseLibS.Graph;
+using BaseLibS.Param;
+using PerseusApi.Network;
+
+namespace PluginNetworkInterop.Python
+{
+    public class NetworkProcessing : PluginNetworkInterop.NetworkProcessing
+    {
+        public override string Name => "=> Python =>";
+        public override string Description => "Run Python Script with Network as input";
+        protected override string CodeFilter => "Python script, *.py | *.py";
+        public override Bitmap2 DisplayImage => null;
+
+        protected override Parameters AddParameters(INetworkData ndata)
+        {
+            return new Parameters();
+        }
+    }
+}
